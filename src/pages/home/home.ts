19066d9e4ae76b6154bb54afa3327155e87c3153
Salah-Ouadi <so285@exeter.ac.uk>
2018-04-16 14:17:13 +0100

    First commit of geoaPP
    Not tested on device yet

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,62 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { Geofence } from '@ionic-native/geofence';
+import { Geolocation } from '@ionic-native/geolocation';
+import { SMS } from '@ionic-native/sms';
+import { ActivePage } from '../active/active';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  radius: number = 100;
+  error: any;
+  success:any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private platform: Platform, private geofence : Geofence, private geolocation : Geolocation, private sms : SMS,) {
+    this.platform.ready().then(() => {
+  geofence.initialize().then(
+    () => console.log('Geofence Plugin Ready'),
+    (err) => console.log(err)
+  );
+
+});
 
   }
+
+  setGeofence(value: number) {
+
+  this.geolocation.getCurrentPosition({
+    enableHighAccuracy: true
+  }).then((resp) => {
+    var longitude = resp.coords.longitude;
+    var latitude = resp.coords.latitude;
+    var radius = value;
+
+    let fence = {
+        id: "myGeofenceID1",
+        latitude:       latitude,
+        longitude:      longitude,
+        radius:         radius,
+        transitionType: 2
+      }
+
+      this.geofence.addOrUpdate(fence).then(
+        () => this.success = true,
+        (err) => this.error = "Failed to add or update the fence."
+      );
+
+      this.geofence.onTransitionReceived().subscribe(resp => {
+        this.sms.send('5555555555', 'OMG She lied, leave her now!');
+      });
+
+      this.navCtrl.push(ActivePage);
+
+
+  }).catch((error) => {
+    this.error = error;
+  });
+}
 
 }
